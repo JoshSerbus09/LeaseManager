1f6c7fc9aeca5e9466031599c576a3fd2a88c818
Josh Serbus <JoshSerbus09@gmail.com>
2021-06-03 10:19:36 -0500

    remove unused files & code cleanup

--- src/index.js
+++ src/index.js
@@ -9,11 +9,3 @@ ReactDOM.render(
     <LeaseManager />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
-// TODO add backlog task for web vitals asseessment
-//  2021.5.27 - jserbus: remove until vitals tracking can be implemented. 
-// reportWebVitals();
